ae1cea4591d7c7ba3a24054cc350ea8661e4ce64
xideme <soopka@gmail.com>
2024-01-11 16:27:29 +0200

    2

--- Praktiline_too_Palgad.py
+++ Praktiline_too_Palgad.py
@@ -4,13 +4,17 @@ palgad=[1200,1200,750,395,1200]
 inimesed=["A","B","C","D","E"]
 
 while True:
-    print("Lisamine-1\nSuurimPalk-3\nSort-5\n")
+    print("Lisamine-1\nKustutamine-2\nSuurimPalk-3\nSort-5\n")
     v=int(input())
     if v==1:
         k=int(input("Mitu inimest lisame? "))
         inimesed,palgad=Lisamine(inimesed, palgad,k)
         for i in range(len(palgad)):
             print(inimesed[i], "saab kätte",palgad[i])
+    elif v==2:
+        inimesed,palgad=Kustutamine(inimesed,palgad)
+        print(inimesed)
+        print(palgad)
     elif v==3:
         maxpalk,nimi=SuurimPalk(inimesed,palgad)
         print(nimi,"saab kätte",maxpalk,"Eur")
