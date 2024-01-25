9fff45ef5bf5d56f0081bec4587a090665fa02b5
xideme <soopka@gmail.com>
2024-01-25 15:01:42 +0200

    read from file

---
+++ Euroopa riigid.py
@@ -0,0 +1,17 @@
+﻿from random import *
+def failist_to_dict(f:str):
+    riik_pealinn={}
+    pealinn_riik={}
+    riigid=[]
+    with open(f,'r',encoding="utf-8-sig") as file:
+        for line in file:
+            if '-' not in line:
+                raise ValueError(f"Строка '{line.strip()}' не содержит символ '-'")
+            k,v=line.strip().split('-')
+            riik_pealinn[k]=v
+            pealinn_riik[v]=k
+            riigid.append(k)
+    return riik_pealinn, pealinn_riik, riigid
+
+riik_pealinn, pealinn_riik, riigid = failist_to_dict("RiigidPealinnad.txt")
+print(riik_pealinn)
